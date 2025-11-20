10dde95019dd378318e6ae108df75b02109869ef
Justus Hammer <justus.hammer@whu.edu>
2025-11-20 19:25:05 +0100

    Dropdown added

--- package_folder/frontend_file.py
+++ package_folder/frontend_file.py
@@ -7,7 +7,7 @@ API_URL = "https://api-nutrimap-1002154750813.europe-west1.run.app/"
 
 st.title("NutriMap - Test")
 
-st.write("Nutrimap test - ignore errors, backend is not properly configured :)")
+st.write("Nutrimap test - this is currently only the flower example")
 
 sepal_length = st.slider('Select a value for Carbs', min_value=0, max_value=4, value=1, step=1)
 sepal_width = st.slider('Select a value for Protein',  min_value=0, max_value=4, value=1, step=1)
@@ -25,3 +25,15 @@ params = {
 response = requests.get(url, params=params).json()
 
 st.write(f"This flower belongs to category {str(response['prediction'])}")
+
+
+st.title("NutriMap - V1")
+
+st.write("Nutrimap test - ignore errors, backend is not properly configured :)")
+
+option = st.selectbox(
+    "Select your source of protein from the options",
+    ["Option A", "Option B", "Option C"]
+)
+
+st.write("Ausgewählt:", option)
